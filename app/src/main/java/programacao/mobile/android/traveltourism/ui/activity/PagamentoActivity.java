dfe8eea2d63b76b4608ce9aad67b02f8480471bd
Jefferson Fabrício <jeffersonfabricioma@gmail.com>
2018-11-06 14:02:02 -0200

    Metodo mostra preço

--- app/src/main/java/programacao/mobile/android/traveltourism/ui/activity/PagamentoActivity.java
+++ app/src/main/java/programacao/mobile/android/traveltourism/ui/activity/PagamentoActivity.java
@@ -2,14 +2,35 @@ package programacao.mobile.android.traveltourism.ui.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import java.math.BigDecimal;
 
 import programacao.mobile.android.traveltourism.R;
+import programacao.mobile.android.traveltourism.model.Pacote;
+import programacao.mobile.android.traveltourism.util.MoedaUtil;
 
 public class PagamentoActivity extends AppCompatActivity {
+
+    public static final String TITULO_APPBAR = "Pagamento";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pagamento);
+
+        setTitle(TITULO_APPBAR);
+
+        Pacote pacoteRecife = new Pacote("Recife", "recife_pe", 2, new BigDecimal("243.99"));
+
+        mostraPreco(pacoteRecife);
+
+    }
+
+    private void mostraPreco(Pacote pacote) {
+        TextView preco = findViewById(R.id.pagamento_txtValorCompra);
+        String moedaBrasileira = MoedaUtil
+                .formataParaBrasileiro(pacote.getPreco());
+        preco.setText(moedaBrasileira);
     }
 }
