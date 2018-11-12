d075c691d9fa6c47cc28ce245cfe4f0786f67a16
Jefferson Fabrício <jeffersonfabricioma@gmail.com>
2018-11-12 18:55:40 -0200

    Set's da classe pacote.

--- app/src/main/java/programacao/mobile/android/traveltourism/model/Pacote.java
+++ app/src/main/java/programacao/mobile/android/traveltourism/model/Pacote.java
@@ -5,28 +5,44 @@ import java.math.BigDecimal;
 
 public class Pacote implements Serializable {
 
-    private final String local;
-    private final String img;
-    private final int dias;
-    private final BigDecimal preco;
+    private String local;
+    private String img;
+    private int dias;
+    private BigDecimal preco;
 
     public Pacote(String local, String img, int dias, BigDecimal preco) {
+        setLocal(local);
+        setImg(img);
+        setDias(dias);
+        setPreco(preco);
+    }
+
+    public void setLocal(String local) {
         this.local = local;
-        this.img = img;
-        this.dias = dias;
-        this.preco = preco;
     }
 
     public String getLocal() {
         return local;
     }
 
+    public void setImg(String img) {
+        this.img = img;
+    }
+
     public String getImg() {
         return img;
     }
 
+    public void setDias(int dias) {
+        this.dias = dias;
+    }
+
     public int getDias() {
         return dias;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
     }
 
     public BigDecimal getPreco() {
